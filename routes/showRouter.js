5c441722400d3a36013da6e08a7e6fc879f34ff0
Aashiq <md.ashikuzzaman.1429@gmail.com>
2019-01-28 19:06:24 +0600

    Delete works I guess, need to work with update

--- routes/showRouter.js
+++ routes/showRouter.js
@@ -7,7 +7,7 @@ var Contact = require('../models/contact');
 var Acquiantance = require('../models/acquiantance');
 
 router.get('/', (req, res, next) => {
-  console.log('requested: ' + req.params.id_details);
+  // console.log('requested: ' + req.params.id_details);
   
   Contact.find({ 'owner.name': req.user.username }, (err, contact) => {
     if (err) {
@@ -40,14 +40,46 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/delete', (req, res, next) => {
-  console.log('requested: ' + req.params.id_details);
-  Acquiantance.find({}, (err,data)=>{
-    if(err) console.log(err);
-    else console.log(data);
+  Contact.find({ 'owner.name': req.user.username }, (err, contact) => {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    }
+    else {
+      var i = 0;
+      var index = 0;
+      var exists = false;
+      contact[0].acquiantances.forEach(acquiantance => {
+        if ((acquiantance._id).equals(req.params.id_details)) {
+          index = i;
+          exists = true;
+        }
+        i++;
+      });
+
+      if (exists) {
+        // var foundAcquiantance = contact[0].acquiantances[index];
+        contact[0].acquiantances.splice(index,1);
+        // console.log(foundAcquiantance);
+        contact[0].save((err, data)=>{
+          if(err) console.log(err);
+          else{
+            console.log(data);
+            
+            res.redirect('/contacts');
+          }
+          
+        });        
+        
+        // res.send('deleteing');
+        // res.redirect('/show/', { v_acquiantance: foundAcquiantance });
+      } else {
+        res.send('Something Wrong')
+      }
+    }
+  });
+  // res.send('deleting');
     
-    res.send('deleteing')
-    
-  });  
 });
 
 module.exports = router;
